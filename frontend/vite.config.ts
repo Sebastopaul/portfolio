386e4d4fd7080f10e714df723cab7ac0e90ec8dd
Sébastien Lamard <sebastien.lamard.pro@gmail.com>
2025-10-18 22:02:38 +0200

    chore(front-tests): add tests to frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [angular()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['src/test.setup.ts'],
+    globals: true, // Permet d'importer les fonctions de test de Vitest dans chaque fichier de test sans avoir à les importer manuellement dans chaque fichier de test
+    include: ['src/**/*.spec.ts']
+  }
+});
